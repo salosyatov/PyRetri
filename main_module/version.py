9df522b4a2236de4f823250d5ee436c3cab2ce6e
Vasily Salosyatov <v@salosyatov.ru>
2023-05-04 12:56:34 +0300

    rename module

---
+++ main_module/version.py
@@ -0,0 +1,4 @@
+# GENERATED VERSION FILE
+# TIME: Wed May  3 14:02:05 2023
+__version__ = '0.1.0+ba37546'
+short_version = '0.1.0'
--- pyretri/version.py
+++
@@ -1,4 +0,0 @@
-# GENERATED VERSION FILE
-# TIME: Tue Apr 11 18:48:06 2023
-__version__ = '0.1.0+3faf94e'
-short_version = '0.1.0'
